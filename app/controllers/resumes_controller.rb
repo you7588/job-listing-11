aa06a7f442449a3bc65a3bfcd6fd7cf691457ead
you7588 <yo7588@gmail.com>
2017-07-27 18:54:01 +0800

    add resumes,growth,event to navbar

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,6 +1,9 @@
 class ResumesController < ApplicationController
   before_action :authenticate_user!
 
+  def index
+  end
+
   def new
     @job = Job.find(params[:job_id])
     @resume = Resume.new
--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,8 @@ Rails.application.routes.draw do
     resources :resumes
   end
 
+  resources :resumes
+
 
   root 'welcome#index'
 end
