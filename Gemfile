c96e91443f5863202d035c0abd338cfb5f704b43
you7588 <yo7588@gmail.com>
2017-07-28 16:15:31 +0800

    add annotate_models

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ gem 'devise'
 gem 'simple_form'
 gem 'font-awesome-rails'
 gem 'carrierwave'
+gem 'annotate', git: 'https://github.com/ctran/annotate_models.git', require: false
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: jobs
+#
+#  id               :integer          not null, primary key
+#  title            :string
+#  description      :text
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  wage_upper_bound :integer
+#  wage_lower_bound :integer
+#  contact_email    :string
+#  is_hidden        :boolean          default(TRUE)
+#  source           :string
+#  place            :string
+#  scale            :string
+#
+
 class Job < ApplicationRecord
   validates :title, presence: true
   validates :wage_upper_bound, presence: true
