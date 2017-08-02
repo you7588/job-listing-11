8d3f7629bc8bddc146517698c96dda289ab6ded9
you7588 <yo7588@gmail.com>
2017-08-02 17:07:35 +0800

    current_user_email have 2 language

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,5 @@
 require_relative 'boot'
 
-
-
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
@@ -10,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module JobListing
   class Application < Rails::Application
+
+    config.i18n.default_locale = "zh-CN"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
